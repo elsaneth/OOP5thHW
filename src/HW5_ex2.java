b2401cacbfc74214c9a53779c65def4687bd24e2
elsaneth <elisabeth.suits@gmail.com>
2024-04-26 14:23:14 +0300

    samples as written in exercise description

--- src/HW5_ex2.java
+++ src/HW5_ex2.java
@@ -5,26 +5,36 @@ import Tooted.Piim;
 
 public class HW5_ex2 {
     public static void main(String[] args) {
-        Juust juust1 = new Juust("Eesti Juust", 7.5);
-        Juust juust2 = new Juust("Atleet", 6);
-        Piim piim1 = new Piim("Alma", 0.65);
-        Kohupiim kohupiim1 = new Kohupiim("Estover", 3);
+        Juust juust1 = new Juust("Juust Gouda", 14.6);
+        Juust juust2 = new Juust("Juust Cheddar", 14.87);
+        Juust juust3 = new Juust("Juust Grand", 16.99);
+        Piim piim1 = new Piim("Piim 2,5%", 1.30);
+        Piim piim2 = new Piim("Kitsepiim", 3.7);
+        Kohupiim kohupiim1 = new Kohupiim("Kohupiim Ricotta", 4);
 
-        Arve arve1 = new Arve("Elias");
+        Arve arve3 = new Arve("Uku");
+        arve3.lisa(juust1, 0.2);
+        arve3.lisa(juust2, 0.1);
+        arve3.lisa(juust3, 0.5);
+        arve3.lisa(piim1, 2.5);
+        arve3.lisa(piim2, 1.5);
+        arve3.lisa(kohupiim1, 2.5);
+
+        Arve arve1 = new Arve("Elisabeth");
         arve1.lisa(juust1, 2.0);
         arve1.lisa(juust2, 2.0);
-        arve1.lisa(piim1, 6);
+        arve1.lisa(piim1, 5);
         arve1.lisa(kohupiim1, 2.0);
 
-        Arve arve2 = new Arve("Eli");
-        arve2.lisa(juust1, 2.0);
-        arve2.lisa(juust2, 2.0);
-        arve2.lisa(piim1, 6);
+        Arve arve2 = new Arve("Siim");
+        arve2.lisa(juust1, 0.5);
+        arve2.lisa(juust3, 1);
+        arve2.lisa(piim2, 2);
         arve2.lisa(kohupiim1, 1.0);
-
 
         arve1.maksa();
         arve2.maksa();
+        arve3.maksa();
 
     }
 }
